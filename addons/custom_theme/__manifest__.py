aef9025741d34b59ddcd0e437dae3c1e52136198
Marcelo Santos <marcelo.net@hotmail.com>
2026-07-29 09:42:44 +0100

    feat(theme): add FACODI learning experience

---
+++ addons/custom_theme/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'FACODI Theme',
+    'summary': 'Digital learning experience for Faculdade Comunitaria Digital',
+    'description': 'A neo-brutalist Website and eLearning theme for FACODI.',
+    'category': 'Theme/Education',
+    'version': '19.0.1.0.0',
+    'sequence': 120,
+    'author': 'Open2Tech',
+    'license': 'LGPL-3',
+    'depends': ['website', 'website_slides'],
+    'data': [
+        'data/generate_primary_template.xml',
+        'data/ir_asset.xml',
+        'views/header.xml',
+        'views/footer.xml',
+        'views/homepage.xml',
+        'views/snippets/facodi_learning_hub.xml',
+    ],
+    'assets': {
+        'website.assets_editor': [
+            'custom_theme/static/src/js/facodi_theme_editor.js',
+        ],
+    },
+    'images': [
+        'static/description/facodi_theme_preview.svg',
+    ],
+    'installable': True,
+}
